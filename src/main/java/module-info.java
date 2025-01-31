4c7bccfd8ec77b4e65478b817dfb2aaf8c6407ae
mllfjn <1245677025@qq.com>
2025-01-31 18:17:42 +0800

    修改chooser前备份

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.mllfjn.simyys {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.google.gson;
+
+
+    opens com.mllfjn.simyys to javafx.fxml;
+    opens com.mllfjn.simyys.starter to com.google.gson;
+    exports com.mllfjn.simyys;
+    exports com.mllfjn.simyys.starter;
+    exports com.mllfjn.simyys.starter.info;
+}
