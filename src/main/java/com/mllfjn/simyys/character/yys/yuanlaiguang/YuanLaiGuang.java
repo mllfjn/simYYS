72d2889e5c383f9972e75c57609c99e383e11d47
mllfjn <1245677025@qq.com>
2025-12-04 16:17:58 +0800

    WIP:refactoring CharacterFinder

---
+++ src/main/java/com/mllfjn/simyys/character/yys/yuanlaiguang/YuanLaiGuang.java
@@ -0,0 +1,12 @@
+package com.mllfjn.simyys.character.yys.yuanlaiguang;
+
+import com.mllfjn.simyys.character.yys.CharacterYYSBase;
+
+public class YuanLaiGuang extends CharacterYYSBase {
+    public static final String CharacterName = "源赖光";
+
+    @Override
+    protected void addOwnSkills() {
+
+    }
+}
